3e5ac9d34359e30f586c6a01a7e0406f2b304f43
M Ichsan <achmadsyahrian1@gmail.com>
2023-11-02 14:09:57 +0700

    Clean Code Route Web

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,29 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index');
-})->middleware('auth');
+// Rute publik yang dapat diakses oleh semua pengguna
+Route::middleware(['guest'])->group(function () {
+    Route::get('/sign-in', [AuthController::class, 'signin'])->name('login');
+    Route::post('/sign-in', [AuthController::class, 'authenticate']);
+    Route::get('/sign-up', [AuthController::class, 'signup']);
+    Route::post('/sign-up', [AuthController::class, 'store']);
+});
 
-// Authentication
-Route::get('/sign-in', [AuthController::class, 'signin'])->middleware('guest')->name('login');
-Route::post('/sign-in', [AuthController::class, 'authenticate'])->middleware('guest');
-Route::post('/sign-out', [AuthController::class, 'signout'])->middleware('auth')->name('sign-out');
 
-Route::get('/sign-up', [AuthController::class, 'signup'])->middleware('guest');
-Route::post('/sign-up', [AuthController::class, 'store'])->middleware('guest');
+// Rute yang memerlukan otentikasi pengguna
+Route::middleware(['auth'])->group(function () {;
+    Route::get('/',  DashboardController::class);
+    Route::post('/sign-out', [AuthController::class, 'signout'])->name('sign-out');
 
-// Users
-Route::resource('/user', UserController::class)->middleware('auth')->names([
-    'index' => 'user',
-    'show' => 'user',
-]);
-Route::patch('/user/activated/{user:id}', [UserController::class, 'activated'])->middleware('auth')->name('activate');
-Route::patch('/user/disable/{user:id}', [UserController::class, 'disable'])->middleware('auth')->name('disable');
+    // User
+    Route::resource('/user', UserController::class)->names([
+        'index' => 'user',
+        'show' => 'user',
+    ]);
+    Route::patch('/user/activate/{user:id}', [UserController::class, 'activate'])->name('user.activate');
+    Route::patch('/user/disable/{user:id}', [UserController::class, 'disable'])->name('user.disable');
+});
